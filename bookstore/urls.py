46d1ce03ef57a9a5472c4131b8e2304f057ba906
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-10-24 14:58:35 -0300

    atulização

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 
+from bookstore import views
+
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
