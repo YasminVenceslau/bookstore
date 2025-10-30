624d65fe5c443ed397866cdcd751173ae4a30c4a
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-10-29 23:46:55 -0300

    ok

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -77,7 +77,7 @@ if "PYTHONANYWHERE_DOMAIN" in os.environ:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": "/home/BlackRosesS2/bookstore/db.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else:
